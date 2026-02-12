58bfb439526c8f222418849658b6fe9797ed184a
Imam Hannani <imamhannani75@gmail.com>
2026-02-13 05:08:53 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,12 +1,12 @@
 package latihanInheritance;
 public class App {
     public static void main(String[] args) {
-        bujurSangkar bujur = new bujurSangkar("Bujur Sangkar", 5);
+        bujurSangkar bujur = new bujurSangkar("Bujur Sangkar", 10);
         bujur.info();
         System.out.println ("\nLuas : " + bujur.luas());
         System.out.println();
 
-        lingkaran l = new lingkaran ("lingkaran", 7);
+        lingkaran l = new lingkaran ("lingkaran", 14);
         l.info();
         System.out.println ("\nLuas : " + l.luas());
     }
